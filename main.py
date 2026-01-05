cdf120046c41b51c602f66bed64bc5f91653f9fe
elena.an.grigoryeva <elena.an.grigoryeva@lenta.com>
2026-01-05 11:24:00 +0400

    add profession

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name, profession):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f'Hi, {name}, you are {profession}')  # Press F9 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
